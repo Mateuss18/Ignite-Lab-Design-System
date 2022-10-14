dc6eabefadb2907d6cec8138c054d7a2c3bd53a5
MateusLopes <mateus20.lopes02@gmail.com>
2022-10-13 23:42:03 -0300

    feat: criando projeto react + ts + vite

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+      <h1>Ola mundo</h1>
+      <button className="bg-black font-black text-2x1">Casa grande</button>
+    </>
+  )
+}
