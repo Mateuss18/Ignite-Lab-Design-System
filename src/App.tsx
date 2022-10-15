a34f8894ef196ac27ed734b7b303db0ab48b91b0
MateusLopes <mateus20.lopes02@gmail.com>
2022-10-14 23:59:28 -0300

    feat: criando componente de texto

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import './styles/global.css';
 
 export function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-      <button className="bg-black font-black text-4xl text-red-600">Hello World</button>
-    </>
+    <span className='text-gray-100'>Eu sou um h1</span>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,24 @@ module.exports = {
       './src/**/*.tsx'
     ],
     theme: {
+        fontSize: {
+            xs: 14,
+            sm: 16,
+            md: 18,
+            lg: 24,
+            '2xl': 32,
+        },
+
+        colors: {
+            'gray-900': '#121214',
+            'gray-800': '#202024',
+            'gray-700': '#7C7C8A',
+            'gray-200': '#C4C4C4',
+            'gray-100': '#E1E1E6',
+
+            'red-900': '#F33535',
+            'red-500': '#F56767',
+        },
         extend: {
             fontFamily: {
                 sans: 'Inter, sans-serif'
